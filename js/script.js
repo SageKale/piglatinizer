85f77e9478d3866f7e09077261a988f7918076d3
SageKale <aalonbenjamin@gmail.com>
2019-11-06 23:25:08 +0000

    Everything

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("#button").click(function(){
+    let userInput=$("input").val();
+$(".road").html(userInput);
+});
 
 
 
